658a26764fbef19e1cb93b8a951738a4fdcd72b1
Atul <atulchavan528@gmail.com>
2023-10-23 18:05:04 +0530

    enabling headless mode for chrome browser

--- selenium-framework/src/test/java/selenium_framework/TestComponents/BaseTest.java
+++ selenium-framework/src/test/java/selenium_framework/TestComponents/BaseTest.java
@@ -7,17 +7,18 @@ import java.time.Duration;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import jdk.internal.org.jline.utils.Log;
 import selenium_framework.pageobjects.LandingPage;
 
 public class BaseTest {
@@ -33,9 +34,14 @@ public class BaseTest {
 		String browserName = System.getProperty("browser") != null ? System.getProperty("browser") : prop.getProperty("browser");
 		
 		try {
-			if(browserName.equalsIgnoreCase("chrome")) {
+			if(browserName.contains("chrome")) {
+				ChromeOptions options = new ChromeOptions();
 				WebDriverManager.chromedriver().setup();
-				driver = new ChromeDriver();
+				if(browserName.contains("Headless")) {
+					options.addArguments("headless");
+				}
+				driver = new ChromeDriver(options);
+				driver.manage().window().setSize(new Dimension(1440, 900));
 			}else if(browserName.equalsIgnoreCase("firefox")) {
 				WebDriverManager.firefoxdriver().setup();
 				driver = new FirefoxDriver();
